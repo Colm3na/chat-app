0b4e27e669f30659a39583313af61b4c049cc795
anagutjor <anagjordano@gmail.com>
2019-03-05 17:35:01 +0100

    create auth component modules

---
+++ src/app/modules/auth-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthTabsComponent } from '../components/auth-tabs/auth-tabs.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthTabsComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule {}
