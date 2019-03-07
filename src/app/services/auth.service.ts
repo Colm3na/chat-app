2d5b050f8d7397a1f96a21e8d6464d81383edd7b
anagutjor <anagjordano@gmail.com>
2019-03-07 21:26:31 +0100

    improve signup service urls

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+const BASEURL = 'http://localhost:3000/api/chatapp/';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private authUrl = 'http://localhost:3000/api/chatapp/register';
-
   constructor( private http: HttpClient ) { }
 
-  signUp(user) {
-    return this.http.post(this.authUrl, user);
+  signUp(body) {
+    return this.http.post(`${BASEURL}/register`, body);
   }
 }
