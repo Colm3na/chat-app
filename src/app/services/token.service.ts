4abeec1c9d313dd3c53bf7563baa7fffda815eea
anagutjor <anagjordano@gmail.com>
2019-03-11 14:15:48 +0100

    add token service

---
+++ src/app/services/token.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor( private cookieService: CookieService ) { }
+
+  getToken() {
+    return this.cookieService.get('chat-token');
+  }
+
+  setToken(token) {
+    return this.cookieService.set('chat-token', token);
+  }
+
+  deleteToken() {
+    return this.cookieService.delete('chat-token');
+  }
+
+  getPayload() {
+    let token = this.getToken();
+    let payload;
+    if (token) {
+      payload = token.split('.')[1];
+      payload = JSON.parse(window.atob(payload));
+    }
+    return payload.data;
+  }
+}
