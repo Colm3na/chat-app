941d0245d9213de0364feab5a9099f794d611e2c
anagutjor <anagjordano@gmail.com>
2019-03-15 19:55:39 +0100

    add users service

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const BASEURL = 'http://localhost:3000/api/chatapp';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor( private http: HttpClient ) { }
+
+  getAllUsers() {
+    this.http.get(`${BASEURL}/users`);
+  }
+
+  getUser(id) {
+    this.http.get(`${BASEURL}/user/:${id}`);
+  }
+
+  getUserByUsername(username) {
+    this.http.get(`${BASEURL}/username/${username}`);
+  }
+}
