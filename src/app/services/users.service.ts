71e66f9d594d0453da20ee8bd874b9f260c0d03d
anagutjor <anagjordano@gmail.com>
2019-03-18 11:55:57 +0100

    add missing returns

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 const BASEURL = 'http://localhost:3000/api/chatapp';
 
@@ -11,14 +12,14 @@ export class UsersService {
   constructor( private http: HttpClient ) { }
 
   getAllUsers() {
-    this.http.get(`${BASEURL}/users`);
+    return this.http.get(`${BASEURL}/users`);
   }
 
   getUser(id) {
-    this.http.get(`${BASEURL}/user/${id}`);
+    return this.http.get(`${BASEURL}/user/${id}`);
   }
 
   getUserByUsername(username) {
-    this.http.get(`${BASEURL}/username/${username}`);
+    return this.http.get(`${BASEURL}/username/${username}`);
   }
 }
