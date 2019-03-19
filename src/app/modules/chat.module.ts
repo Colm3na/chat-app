0993bf9b7a71cfe9557bba094fd7d8a5d712b9d9
anagutjor <anagjordano@gmail.com>
2019-03-19 13:03:15 +0100

    add rxjs-compat for backward compatibility

--- src/app/modules/chat.module.ts
+++ src/app/modules/chat.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from '../components/chat/chat.component';
 import { MessageModule } from '../components/message/message.module';
+import { EmojiPickerModule } from 'ng2-emoji-picker';
 
 @NgModule({
   declarations: [ ChatComponent ],
   imports: [
     CommonModule,
     ChatRoutingModule,
-    MessageModule
+    MessageModule,
+    EmojiPickerModule.forRoot(),
   ]
 })
 export class ChatModule { }
