33e31c1c527f5ebdca1d57689f0c74f905de82ae
anagutjor <anagjordano@gmail.com>
2019-03-19 21:58:32 +0100

    correct send function

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -86,24 +86,32 @@ export class MessageComponent implements OnInit {
   }
 
   send() {
-    this.message = '';
-    this.message = this.input.nativeElement.value;
-    this.messagesList.push(this.message);
-    this.socket.emit('new message', this.message);
-    // set back input value to empty string
-    this.input.nativeElement.value = '';
-    // typing to false
-    this.typing = false;
-    // save chat message in DB
-    console.log(this.senderData)
-    this.messageDB = {
-      body: this.message,
-      sender: this.sender,
-      senderId: this.senderData['user'][0]._id
-    }
-    this.messageService.saveMessage(this.messageDB).subscribe( data => {
-      console.log(data);
-    });
+    // with this if statement empty messages are prevented
+    if ( this.input.nativeElement.value !== '' || this.message !== '' ) {
+      // detect if string is a emoji
+      let regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+      // if it is not a emoji then message will be the current input value
+      if (!regex.test(this.message)) {
+        this.message = this.input.nativeElement.value;
+      }
+      this.messagesList.push(this.message);
+      this.socket.emit('new message', this.message);
+      // set back input value to empty string
+      this.input.nativeElement.value = '';
+      // typing to false
+      this.typing = false;
+      // save chat message in DB
+  
+      this.messageDB = {
+        body: this.message,
+        sender: this.sender,
+        senderId: this.senderData['user'][0]._id
+      }
+      this.messageService.saveMessage(this.messageDB).subscribe( data => {
+        console.log(data);
+      });
+      this.message = '';
+    } 
   }
 
 }
