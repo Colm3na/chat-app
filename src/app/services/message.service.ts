e4fc15962fa260431d658863fda5cc5bd32fa55e
anagutjor <anagjordano@gmail.com>
2019-03-26 19:56:29 +0100

    update sendmessage and add getconversationmsg

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 const BASEURL = 'http://localhost:3000/api/chatapp';
 
@@ -9,6 +10,17 @@ const BASEURL = 'http://localhost:3000/api/chatapp';
 export class MessageService {
 
   constructor( private http: HttpClient ) { }
+
+  sendMessage( senderId, receiverId, message ) {
+    return this.http.post(`${BASEURL}/chat-messages/${senderId}/${receiverId}`, message)
+  }
+
+  getConversationMessages( senderId, receiverId ) {
+    let response1 = this.http.get(`${BASEURL}/chat-messages/${senderId}/${receiverId}`);
+    let response2 = this.http.get(`${BASEURL}/chat-messages/${receiverId}/${senderId}`);
+
+    return forkJoin([response1, response2])
+  }
 
   saveMessage(body) {
     return this.http.post(`${BASEURL}/chat-messages`, body)
